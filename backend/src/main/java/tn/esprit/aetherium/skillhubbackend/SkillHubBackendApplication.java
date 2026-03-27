2636800f5f95ba2ba9097339fd5b64c545b2a95c
ArousDev <arasti.arous@gmail.com>
2026-03-27 19:01:43 +0100

    initial project directory

---
+++ backend/src/main/java/tn/esprit/aetherium/skillhubbackend/SkillHubBackendApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.aetherium.skillhubbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillHubBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkillHubBackendApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/tn/esprit/aetherium/skillhubbackend/SkillHubBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.aetherium.skillhubbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillHubBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
